7ab9f8e21241d242ef21a24b93bf8295a21d2723
Martin Evans <23621647@student.uwa.edu.au>
2023-10-13 11:16:41 +0800

    plot.py

--- Qlearning/performance.py
+++ Qlearning/performance.py
@@ -5,6 +5,7 @@ import gym
 from gym.wrappers.frame_stack import FrameStack
 
 import os
+import sys
 
 import numpy as np
 import torch
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     env.reset()
    
 
-    current_log = "steps"
+    current_log = sys.argv[1]
     mario = MarioAgent(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir="./network_log", scratch_dir="./tmp")
     checkpoint = torch.load("mario_net_22.chkpt", map_location=torch.device("cpu"))
     mario.policy_net.load_state_dict(checkpoint["model"])
@@ -56,17 +57,20 @@ if __name__ == "__main__":
         mario = MarioAgent(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir="./network_log", scratch_dir="./tmp")
         print(f"Device: {mario.device}...")
         while time.time() < start_time + 15*(60):
+            print(f"Current Time: {(time.time() - start_time) / 60}")
             current_state = env.reset() 
             while True:
                     action = mario.act(current_state)
 
                     state, reward, terminated, truncated, info = env.step(action)
 
-                    if(info["x_pos"] not in time_dict.keys() and info["x_pos"] >= distance_index):
+                    if((distance_index not in time_dict.keys()) and info["x_pos"] >= distance_index):
                         time_dict[distance_index] = time.time() - start_time
                         distance_index += 200
                     
                     if terminated or info["flag_get"]:
+                        if info["flag_get"]:
+                            print("COMPLETED!!!")
                         break
                     
                     current_state = state
@@ -84,21 +88,21 @@ if __name__ == "__main__":
         x = np.array(x)
         y = np.array(y)
 
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
+        plt.scatter(x,y)
+        plt.ylim(0,None)
 
         plt.title(" Time to reach certain distances.")
         plt.xlabel("Distance (x position)")
         plt.ylabel("Time taken (seconds)")
 
         plt.show()
-        plt.savefig("time_to_reach_certain_distances.png")
+        plt.savefig("time_to_reach_certain_distances2.png")
+
 
 
     if current_log == "steps":
         steps_dict = {}
-        n_episodes = 10
+        n_episodes = 1000
         step_count = 0
         for episode in range(n_episodes):
             print(f"Episode: {episode}")
@@ -109,30 +113,28 @@ if __name__ == "__main__":
                     state, reward, terminated, truncated, info = env.step(action)
                     step_count += 1
 
-                    if(info["x_pos"] >= distance_index):
+                    if(distance_index not in steps_dict.keys() and info["x_pos"] >= distance_index):
                         steps_dict[distance_index] = step_count
                         distance_index += 200
                     
                     if terminated or info["flag_get"]:
+                        episode = n_episodes
                         break
                     
                     current_state = state
         # plot matplotlib
+
+        print(steps_dict)
         values = sorted(steps_dict.items())
         x, y = zip(*values)
 
-        print(x)
-
-        print()
-
-        print(y)
         x = np.array(x)
         y = np.array(y)
 
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
+        plt.scatter(x,y)
+        plt.ylim(0,None)
 
+        plt.scatter(x, y)
         plt.title(" Numbers of moves taken to reach certain distances.")
         plt.xlabel("Distance (x position)")
         plt.ylabel("Number of moves taken (step)")
@@ -141,9 +143,10 @@ if __name__ == "__main__":
 
     if current_log == "deaths":
         deaths_dict = {}
-        n_episodes = 10
+        n_episodes = 1000
         death_count = 0
         for episode in range(n_episodes):
+            print(f"Episode: {episode}")
             current_state = env.reset()
             while True:
                     action = mario.act(current_state)
@@ -160,15 +163,15 @@ if __name__ == "__main__":
                     
                     current_state = state
         # plot matplotlib
+        print(deaths_dict)
         values = sorted(deaths_dict.items())
         x, y = zip(*values)
 
         x = np.array(x)
         y = np.array(y)
 
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
+        plt.scatter(x,y)
+        plt.ylim(0,None)
 
         plt.title(" Deaths required to reach certain distances.")
         plt.xlabel("Distance (x position)")
@@ -179,8 +182,12 @@ if __name__ == "__main__":
     if current_log == "score":
         # reset env
         score_dict = {}
-        n_episodes = 100
+        value_dict = {}
+        n_episodes = 5000
         for episode in range(n_episodes):
+            distance_index = 200
+            if episode % 50 == 0:
+                 print(f"Ep: {episode}")
             current_state = env.reset()
             while True:
                     action = mario.act(current_state)
@@ -188,23 +195,26 @@ if __name__ == "__main__":
                     state, reward, terminated, truncated, info = env.step(action)
 
                     if(info["x_pos"] >= distance_index):
-                        score_dict[distance_index] = info["score"]
+                        value_dict[distance_index] = []
+                        value_dict[distance_index].append(info["score"])
+                        score_dict[distance_index] = sum(value_dict[distance_index]) / len(value_dict[distance_index])
                         distance_index += 200
+
                     
-                    if terminated or info["flag_get"] or info["life"] < 3:
+                    if terminated or info["flag_get"] or info["life"] < 2:
                         break
                     
                     current_state = state
         # plot matplotlib
+        print(score_dict)
         values = sorted(score_dict.items())
         x, y = zip(*values)
 
         x = np.array(x)
         y = np.array(y)
 
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
+        plt.scatter(x,y)
+        plt.ylim(0,None)
     
 
         plt.title("Mean score over distance")
@@ -220,7 +230,8 @@ if __name__ == "__main__":
 
         # Setup Agent
         print(f"Device: {mario.device}...")
-        while time.time() < start_time + 15(60):
+        while time.time() < start_time + 30*(60):
+            print(f"Current Time: {(time.time() - start_time) / 60}")
             current_state = env.reset() 
             while True:
                     action = mario.act(current_state)
@@ -236,32 +247,20 @@ if __name__ == "__main__":
                     
                     current_state = state
         # plot matplotlib
-        values = sorted(time_dict.items())
-        x, y = zip(*values)
-
-        x = np.array(x)
-        y = np.array(y)
-
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
-
-        plt.title(" Time to reach certain distances.")
-        plt.xlabel("Distance (x position)")
-        plt.ylabel("Time taken (seconds)")
-        plt.show()
-        plt.savefig("time_to_reach_certain_distances.png")
+        print(time_taken)
     
     if current_log == "memory":
         time_taken = 0
         start_time = time.time()
 
-        net_memory = 0
-        mm_count = 0
+        net_memory = []
+        avg = 0
 
         # Setup Agent
         print(f"Device: {mario.device}...")
-        while time.time() < start_time + 15(60):
+        while time.time() < start_time + 15*(60):
+            if(len(net_memory) > 0):
+                print(f"Current Avg: {sum(net_memory) / len(net_memory)}")
             current_state = env.reset() 
             while True:
                     action = mario.act(current_state)
@@ -269,7 +268,7 @@ if __name__ == "__main__":
                     state, reward, terminated, truncated, info = env.step(action)
 
                     if info["flag_get"]:
-                        net_memory = net_memory / mm_count
+                        avg = sum(net_memory) / len(net_memory)
                         break
 
                     if terminated:
@@ -277,18 +276,21 @@ if __name__ == "__main__":
                     
                     current_state = state
 
-                    net_memory += psutil.virtual_memory().percent
-                    mm_count += 1
+                    net_memory.append(psutil.virtual_memory().percent)
+                    avg = sum(net_memory) / len(net_memory)
         # plot matplotlib
+        print(f"Average: {avg}")
         values = sorted(time_dict.items())
         x, y = zip(*values)
 
         x = np.array(x)
         y = np.array(y)
 
-        a, b = np.polyfit(x,y,1)
-        plt.scatter(x, y)
-        plt.plot(x,a*x+b)
+        x = np.array(x)
+        y = np.array(y)
+
+        plt.scatter(x,y)
+        plt.ylim(0,None)
 
         plt.title(" Time to reach certain distances.")
         plt.xlabel("Distance (x position)")
